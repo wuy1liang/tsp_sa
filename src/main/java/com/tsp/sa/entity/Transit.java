e8ac7749e2e70ce8c9e9196ca4585264609ca296
wuyiliang <tuibii@163.com>
2019-10-12 11:51:48 +0800

    json工具类

--- src/main/java/com/tsp/sa/entity/Transit.java
+++ src/main/java/com/tsp/sa/entity/Transit.java
@@ -10,17 +10,34 @@ import java.util.List;
 @Data
 public class Transit {
 
+    /**
+     * 价格
+     */
     private String cost;
 
+    /**
+     * 预期时间
+     */
     private String duration;
 
+    /**
+     * 是否是夜班车
+     * 0：非夜班车；1：夜班车
+     */
     private String nightflag;
 
+    /**
+     * 总步行距离
+     */
     private String walking_distance;
+
+    /**
+     * 换乘路段列表
+     */
+    private List<Segment> segments;
 
     private String distance;
 
     private String missed;
 
-    private List<Segment> segments;
 }
--- src/main/java/com/tsp/sa/utils/JsonUtil.java
+++ src/main/java/com/tsp/sa/utils/JsonUtil.java
@@ -3,13 +3,10 @@ package com.tsp.sa.utils;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.tsp.sa.entity.GaodeTransitDirectionData;
-import com.tsp.sa.entity.Route;
-import com.tsp.sa.entity.Transit;
+import com.tsp.sa.entity.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 
 public class JsonUtil {
 
@@ -24,6 +21,7 @@ public class JsonUtil {
         data.setCount(object.getString("count"));
         data.setInfocode(object.getString("infocode"));
         JSONObject routeJson = object.getJSONObject("route");
+
         Route route = json2Route(routeJson);
         data.setRoute(route);
 
@@ -41,8 +39,10 @@ public class JsonUtil {
         JSONArray transitArray = routeJson.getJSONArray("transits");
         List<Transit> transits = new ArrayList<>();
         for (int i = 0;i < transitArray.size(); i++ ){
+
             Transit transit = json2Transit(transitArray.getJSONObject(i));
             transits.add(transit);
+
         }
         route.setTransits(transits);
 
@@ -52,7 +52,51 @@ public class JsonUtil {
     public static Transit json2Transit(JSONObject jsonObject) {
         Transit transit = new Transit();
 
+        transit.setCost(jsonObject.getString("cost"));
+        transit.setDistance(jsonObject.getString("duration"));
+        transit.setNightflag(jsonObject.getString("nightflag"));
+        transit.setWalking_distance(jsonObject.getString("walking_distance"));
+
+        JSONArray segmentArray = jsonObject.getJSONArray("segments");
+        List<Segment> segments = new ArrayList<>();
+        for (int i = 0;i < segmentArray.size();i++){
+
+            Segment segment = json2Segment(segmentArray.getJSONObject(i));
+            segments.add(segment);
+
+        }
+        transit.setSegments(segments);
 
         return transit;
+    }
+
+    public static Segment json2Segment(JSONObject jsonObject) {
+
+        Segment segment = new Segment();
+
+        Walking walking = json2Walking(jsonObject.getJSONObject("walking"));
+        segment.setWalking(walking);
+
+        JSONObject busJson = jsonObject.getJSONObject("bus");
+        List<Busline> buslineList = new ArrayList<>();
+        if (busJson != null){
+            JSONArray buslines = busJson.getJSONArray("buslines");
+            for (int i = 0;i < buslines.size();i++){
+                Busline busline = json2Busline(buslines.getJSONObject(i));
+                buslineList.add(busline);
+            }
+        }
+        segment.setBuslines(buslineList);
+
+
+        return segment;
+    }
+
+    private static Busline json2Busline(JSONObject jsonObject) {
+        return null;
+    }
+
+    private static Walking json2Walking(JSONObject walking) {
+        return null;
     }
 }
